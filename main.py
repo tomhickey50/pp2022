411b5cb2dad4ead88e695708897906ef154e1fb8
Tom <tomhickey50@gmail.com>
2023-12-10 09:18:25 -0600

    changes

--- main.py
+++ main.py
@@ -98,4 +98,4 @@ def get_static_json(path):
 
 if __name__ == "__main__":
     print("running py app")
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=8080, debug=True)
